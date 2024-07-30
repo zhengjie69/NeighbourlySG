8f56c78363cc8428e8ff267a66be07198597c380
Siow Jia Yie <jiayie.siow@ncs.com.sg>
2024-07-31 01:24:09 +0800

    add getter and setter

--- neighbourlysg-backend/src/main/java/com/nusiss/neighbourlysg/entity/Profile.java
+++ neighbourlysg-backend/src/main/java/com/nusiss/neighbourlysg/entity/Profile.java
@@ -31,4 +31,54 @@ public class Profile {
 
     @Column(name = "contact_number", length = 8)
     private Long contactNumber;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Long getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(Long contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+    
+    
 }
