213c5b7773395ca680f097c74138660b24cabdf5
zhengjie69 <zhengjie69@users.noreply.github.com>
2024-08-10 14:23:51 +0000

    deploy: a2e2c50c144273e05a7c370a350aa96f1648edc3

--- neighbourlysg-backend/src/main/java/com/nusiss/neighbourlysg/repository/ProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nusiss.neighbourlysg.repository;
-
-import com.nusiss.neighbourlysg.entity.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProfileRepository extends JpaRepository<Profile, Long> {
-}
