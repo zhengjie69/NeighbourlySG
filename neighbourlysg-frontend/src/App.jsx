8d9410ff9dd97dc7715ccf3f010faa1f5c47390d
Koh Guan Chin James <110974244+gcjk768@users.noreply.github.com>
2024-08-11 18:14:04 +0800

    ProfileSettingPage

--- neighbourlysg-frontend/src/App.jsx
+++ neighbourlysg-frontend/src/App.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ResidentLogin from './auth/Resident/ResidentLogin';
 import RegisterPage from './auth/Resident/RegisterPage';
-import ResidentMainPage from './auth/Resident/ResidentMainPage'; // Updated path to ResidentMainPage
+import ResidentMainPage from './auth/Resident/ResidentMainPage';
+import ProfileSettingsPage from './auth/Resident/ProfileSettingsPage';
 
 const App = () => {
     return (
@@ -12,7 +13,8 @@ const App = () => {
                 <Route path="/" element={<ResidentLogin />} />
                 <Route path="/ResidentLogin" element={<ResidentLogin />} />
                 <Route path="/Register" element={<RegisterPage />} />
-                <Route path="/ResidentMainPage" element={<ResidentMainPage />} /> {/* Updated route for ResidentMainPage */}
+                <Route path="/ResidentMainPage" element={<ResidentMainPage />} />
+                <Route path="/ProfileSettings" element={<ProfileSettingsPage />} />
             </Routes>
         </Router>
     );
