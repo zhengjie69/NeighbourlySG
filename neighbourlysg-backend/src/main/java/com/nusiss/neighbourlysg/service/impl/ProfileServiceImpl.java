ceb68a4d0275f04bf123c33b9a83f87c6e56d3c6
Siow Jia Yie <jiayie.siow@ncs.com.sg>
2024-08-13 01:31:49 +0800

    RegisterAccountResident

--- neighbourlysg-backend/src/main/java/com/nusiss/neighbourlysg/service/impl/ProfileServiceImpl.java
+++ neighbourlysg-backend/src/main/java/com/nusiss/neighbourlysg/service/impl/ProfileServiceImpl.java
@@ -1,74 +1,66 @@
 package com.nusiss.neighbourlysg.service.impl;
 
-import com.nusiss.neighbourlysg.dto.ProfileDto;
-import com.nusiss.neighbourlysg.entity.Profile;
-import com.nusiss.neighbourlysg.exception.ResourceNotFoundException;
-import com.nusiss.neighbourlysg.repository.ProfileRepository;
-import com.nusiss.neighbourlysg.service.ProfileService;
-import com.nusiss.neighbourlysg.mapper.ProfileMapper;
-import lombok.AllArgsConstructor;
+import java.util.Optional;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import com.nusiss.neighbourlysg.dto.LoginRequestDTO;
+import com.nusiss.neighbourlysg.dto.ProfileDto;
+import com.nusiss.neighbourlysg.entity.Profile;
+import com.nusiss.neighbourlysg.exception.EmailInUseException;
+import com.nusiss.neighbourlysg.exception.PasswordWrongException;
+import com.nusiss.neighbourlysg.exception.UserNotExistedException;
+import com.nusiss.neighbourlysg.mapper.ProfileMapper;
+import com.nusiss.neighbourlysg.repository.ProfileRepository;
+import com.nusiss.neighbourlysg.service.ProfileService;
 
 @Service
-@AllArgsConstructor
 public class ProfileServiceImpl implements ProfileService {
 
-    private ProfileRepository profileRepository;
-
+    private final ProfileRepository profileRepository;
+    private final ProfileMapper profileMapper;
+    
+    public ProfileServiceImpl(ProfileRepository profileRepository, ProfileMapper profileMapper) {
+    	this.profileRepository=profileRepository;
+    	this.profileMapper=profileMapper;
+    	
+    }
+    
     @Override
     public ProfileDto createProfile(ProfileDto profileDto) {
+    	// Check if user with email already exists, since email is unique
+        if (profileRepository.findByEmail(profileDto.getEmail()).isPresent()) {
+            throw new EmailInUseException();
+        }
 
-        Profile profile = ProfileMapper.mapToProfile(profileDto);
+        // Encrypt the password 
+        //String encodedPassword = passwordEncoder.encode(password);
+	
+        Profile profile = profileMapper.toEntity(profileDto);
         Profile savedProfile = profileRepository.save(profile);
 
-        return ProfileMapper.mapToDto(savedProfile);
+        return profileMapper.toDto(savedProfile);
     }
-
+    
     @Override
-    public ProfileDto getProfileById(Long id) {
-        Profile profile = profileRepository.findById(id)
-                .orElseThrow(() ->
-                        new ResourceNotFoundException("Profile Not Found with the given id: " + id));
+    public ProfileDto login(LoginRequestDTO loginRequestDTO) {
+    	// Check if user with email, check password
+    	
+    	Optional<Profile> profileOp = profileRepository.findByEmail(loginRequestDTO.getEmail());
+    	
+        if (profileOp.isPresent()) {
+            //check password
+        	if(!StringUtils.equals(profileOp.get().getPassword(), loginRequestDTO.getPassword())) {
+        		 throw new PasswordWrongException();
+        	}
+        }else {
+        	throw new UserNotExistedException();
+        } 
+        
+        return profileMapper.toDto(profileOp.get());
 
-        return ProfileMapper.mapToDto(profile);
-    }
-
-    @Override
-    public List<ProfileDto> getAllProfile() {
-        List<Profile> listOfProfile = profileRepository.findAll();
-        return listOfProfile.stream().map(ProfileMapper::mapToDto)
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public ProfileDto updateProfile(Long id, ProfileDto updatedProfileDto) {
-
-        Profile retrievedProfile = profileRepository.findById(id)
-                .orElseThrow(() ->
-                        new ResourceNotFoundException("Profile which want to be updated cannot be found" + id));
-
-        retrievedProfile.setContactNumber(updatedProfileDto.getContactNumber());
-        retrievedProfile.setLastName(updatedProfileDto.getLastName());
-        retrievedProfile.setFirstName(updatedProfileDto.getFirstName());
-        retrievedProfile.setContactNumber(updatedProfileDto.getContactNumber());
-        retrievedProfile.setUserName(updatedProfileDto.getUserName());
-
-        Profile updatedProfile = profileRepository.save(retrievedProfile);
-
-        return ProfileMapper.mapToDto(updatedProfile);
-    }
-
-    @Override
-    public void deleteProfile(Long id) {
-
-        Profile retrievedProfile = profileRepository.findById(id)
-                .orElseThrow(() ->
-                        new ResourceNotFoundException("Profile which want to be deleted cannot be found" + id));
-
-        profileRepository.deleteById(id);
+        
     }
 
 }
