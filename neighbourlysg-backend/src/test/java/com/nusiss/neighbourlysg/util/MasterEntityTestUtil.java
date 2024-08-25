02698839e00a3384b635e70e5f8f644433ce2f8b
Siow Jia Yie <jiayie.siow@ncs.com.sg>
2024-08-25 22:48:01 +0800

    MasterEntityTestUtil

--- neighbourlysg-backend/src/test/java/com/nusiss/neighbourlysg/util/MasterEntityTestUtil.java
+++ neighbourlysg-backend/src/test/java/com/nusiss/neighbourlysg/util/MasterEntityTestUtil.java
@@ -1,6 +1,9 @@
 package com.nusiss.neighbourlysg.util;
 
 import com.nusiss.neighbourlysg.entity.Profile;
+import com.nusiss.neighbourlysg.entity.Role;
+
+import java.util.Arrays;
 
 public final class MasterEntityTestUtil {
 
@@ -9,9 +12,16 @@ public final class MasterEntityTestUtil {
         profile.setConstituency("con");
         profile.setEmail("email");
         profile.setId(1L);
-        profile.setIsAdmin(true);
+        profile.setRoles(Arrays.asList(createRoleEntity()));
         profile.setName("name");
         profile.setPassword("password");
         return profile;
+    }
+
+    public static Role createRoleEntity(){
+        Role role = new Role();
+        role.setId(1);
+        role.setName("USER");
+        return role;
     }
 }
