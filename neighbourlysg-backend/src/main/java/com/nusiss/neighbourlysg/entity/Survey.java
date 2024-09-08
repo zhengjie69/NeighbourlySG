ea2096d4d697770dfc5478d58dff1f1ae7dd515a
Siow Jia Yie <jiayie.siow@ncs.com.sg>
2024-09-08 22:45:28 +0800

    Survey.java

---
+++ neighbourlysg-backend/src/main/java/com/nusiss/neighbourlysg/entity/Survey.java
@@ -0,0 +1,59 @@
+package com.nusiss.neighbourlysg.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+
+import java.util.List;
+
+@Entity
+@Table(name = "survey")
+public class Survey {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "survey", orphanRemoval = true)
+    private List<Question> questions;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+}
