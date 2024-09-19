7e003b767aae95fe00a37754ca4005d6ee8c076d
Siow Jia Yie <jiayie.siow@ncs.com.sg>
2024-09-20 00:48:28 +0800

    role controller

--- neighbourlysg-backend/src/main/java/com/nusiss/neighbourlysg/controller/RoleController.java
+++ neighbourlysg-backend/src/main/java/com/nusiss/neighbourlysg/controller/RoleController.java
@@ -3,6 +3,8 @@ package com.nusiss.neighbourlysg.controller;
 import com.nusiss.neighbourlysg.dto.RoleDto;
 import com.nusiss.neighbourlysg.service.RoleService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +21,7 @@ public class RoleController {
     }
 
     @GetMapping("/roles")
+    @PreAuthorize("hasRole('USER') or hasRole('ORGANISER') or hasRole('ADMIN')")
     public ResponseEntity<List<RoleDto>> getAllRoles() {
         List<RoleDto> roles = roleService.getAllRoles();
         return ResponseEntity.ok(roles); // Return the list of roles with 200 OK status
