f2996e1d2b5dd02c6ec50215f29e36ea759e1485
Siow Jia Yie <jiayie.siow@ncs.com.sg>
2024-09-20 00:47:27 +0800

    authcontroller

--- neighbourlysg-backend/src/main/java/com/nusiss/neighbourlysg/controller/AuthenticationController.java
+++ neighbourlysg-backend/src/main/java/com/nusiss/neighbourlysg/controller/AuthenticationController.java
@@ -1,28 +1,85 @@
 package com.nusiss.neighbourlysg.controller;
+import com.nusiss.neighbourlysg.dto.JwtResponse;
+import com.nusiss.neighbourlysg.repository.RoleRepository;
+import com.nusiss.neighbourlysg.security.jwt.JwtUtils;
+import com.nusiss.neighbourlysg.security.services.UserDetailsImpl;
+import com.nusiss.neighbourlysg.service.ProfileService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nusiss.neighbourlysg.dto.LoginRequestDTO;
 import com.nusiss.neighbourlysg.dto.ProfileDto;
-import com.nusiss.neighbourlysg.service.ProfileService;
+
+import javax.management.relation.RoleNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 @RestController
+@RequestMapping("/api/auth")
 public class AuthenticationController {
 
-    
-  
+    private final AuthenticationManager authenticationManager;
+
+    private final RoleRepository roleRepository;
+
+    private final PasswordEncoder encoder;
+
+    private final JwtUtils jwtUtils;
+
     private final ProfileService profileService;
-    
-    public AuthenticationController(ProfileService profileService) {
-    	
-    	this.profileService=profileService;
+
+    public AuthenticationController(AuthenticationManager authenticationManager, RoleRepository roleRepository,
+                                    PasswordEncoder encoder, JwtUtils jwtUtils, ProfileService profileService) {
+        this.authenticationManager = authenticationManager;
+        this.roleRepository = roleRepository;
+        this.encoder = encoder;
+        this.jwtUtils = jwtUtils;
+        this.profileService = profileService;
     }
-    
-	@PostMapping("/login")
-    public ResponseEntity<ProfileDto> login(@RequestBody LoginRequestDTO loginRequest) {
-        ProfileDto profile = profileService.login(loginRequest);
-        return ResponseEntity.ok(profile);
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(@RequestBody LoginRequestDTO loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(new JwtResponse(jwt,
+                userDetails.getId(),
+                userDetails.getUsername(),
+                userDetails.getEmail(),
+                roles));
     }
+
+    //Create Profile REST API
+    @PostMapping("/register")
+    public ResponseEntity<ProfileDto> createProfile(@RequestBody ProfileDto profileDto) {
+        try {
+            ProfileDto profile = profileService.createProfile(profileDto);
+            return ResponseEntity.ok(profile);
+        } catch (RoleNotFoundException e) {
+            // Return a response with a 400 Bad Request status and error message
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+    }
+
+
+
 }
