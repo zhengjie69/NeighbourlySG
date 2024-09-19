7108c52cdbfe4f72474cd1cf311b471b7d3b0295
Siow Jia Yie <jiayie.siow@ncs.com.sg>
2024-09-20 00:48:39 +0800

    survey controller

--- neighbourlysg-backend/src/main/java/com/nusiss/neighbourlysg/controller/SurveyController.java
+++ neighbourlysg-backend/src/main/java/com/nusiss/neighbourlysg/controller/SurveyController.java
@@ -4,6 +4,7 @@ package com.nusiss.neighbourlysg.controller;
 import com.nusiss.neighbourlysg.dto.SurveyDTO;
 import com.nusiss.neighbourlysg.service.SurveyService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,6 +20,7 @@ public class SurveyController {
     }
 
     @PostMapping("/createSurvey")
+    @PreAuthorize("hasRole('USER')")
     public ResponseEntity<SurveyDTO> createSurvey(@RequestBody SurveyDTO surveyDTO) {
         return ResponseEntity.ok(surveyService.createSurvey(surveyDTO));
     }
@@ -28,7 +30,14 @@ public class SurveyController {
         return ResponseEntity.ok(surveyService.getAllSurveys());
     }
 
+    @GetMapping("/getAllSurveys2")
+    @PreAuthorize("hasRole('ORGANISER')")
+    public ResponseEntity<List<SurveyDTO>> getAllSurveys2() {
+        return ResponseEntity.ok(surveyService.getAllSurveys());
+    }
+
     @GetMapping("/survey/{id}")
+    @PreAuthorize("hasRole('USER') or hasRole('ORGANISER') or hasRole('ADMIN')")
     public ResponseEntity<SurveyDTO> getSurveyById(@PathVariable Long id) {
         return surveyService.getSurveyById(id)
                 .map(ResponseEntity::ok)
