8d1e4f79c91af0a62709d651d538f6b698b81dc6
Siow Jia Yie <jiayie.siow@ncs.com.sg>
2024-09-20 00:53:21 +0800

    UserRepository.java

---
+++ neighbourlysg-backend/src/main/java/com/nusiss/neighbourlysg/repository/UserRepository.java
@@ -0,0 +1,13 @@
+//package com.nusiss.neighbourlysg.repository;
+//
+//
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface UserRepository extends JpaRepository<User, Long> {
+//  Optional<User> findByUsername(String username);
+//
+//  Boolean existsByUsername(String username);
+//
+//  Boolean existsByEmail(String email);
+//}
