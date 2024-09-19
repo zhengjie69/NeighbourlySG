ab98ca7115b7000b8c4545385b5936e4ff694b51
Siow Jia Yie <jiayie.siow@ncs.com.sg>
2024-09-20 00:54:17 +0800

    SecurityUtils.java

---
+++ neighbourlysg-backend/src/main/java/com/nusiss/neighbourlysg/security/jwt/SecurityUtils.java
@@ -0,0 +1,18 @@
+package com.nusiss.neighbourlysg.security.jwt;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class SecurityUtils {
+
+
+    public static String getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+            return ((UserDetails) authentication.getPrincipal()).getUsername();
+        } else {
+            return "Anonymous";
+        }
+    }
+}
