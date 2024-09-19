3cb42f1cf11165da493144ddf271ea5c1e1d1bcb
Siow Jia Yie <jiayie.siow@ncs.com.sg>
2024-09-20 00:51:04 +0800

    profile service impl

--- neighbourlysg-backend/src/main/java/com/nusiss/neighbourlysg/service/impl/ProfileServiceImpl.java
+++ neighbourlysg-backend/src/main/java/com/nusiss/neighbourlysg/service/impl/ProfileServiceImpl.java
@@ -1,19 +1,21 @@
 package com.nusiss.neighbourlysg.service.impl;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
+import com.nusiss.neighbourlysg.common.RoleConstants;
 import com.nusiss.neighbourlysg.dto.RoleAssignmentDto;
 import com.nusiss.neighbourlysg.entity.Role;
 import com.nusiss.neighbourlysg.exception.*;
 import com.nusiss.neighbourlysg.repository.RoleRepository;
-import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.nusiss.neighbourlysg.dto.LoginRequestDTO;
 import com.nusiss.neighbourlysg.dto.ProfileDto;
 import com.nusiss.neighbourlysg.entity.Profile;
 import com.nusiss.neighbourlysg.mapper.ProfileMapper;
@@ -29,38 +31,68 @@ public class ProfileServiceImpl implements ProfileService {
     private final ProfileRepository profileRepository;
     private final ProfileMapper profileMapper;
     private final RoleRepository roleRepository;
+    private final PasswordEncoder encoder;
 
-    public ProfileServiceImpl(ProfileRepository profileRepository, ProfileMapper profileMapper, RoleRepository roleRepository) {
+    public ProfileServiceImpl(ProfileRepository profileRepository, ProfileMapper profileMapper, RoleRepository roleRepository,
+                              PasswordEncoder encoder) {
     	this.profileRepository=profileRepository;
     	this.profileMapper=profileMapper;
         this.roleRepository = roleRepository;
+        this.encoder = encoder;
     }
     
     @Override
     @Transactional
     public ProfileDto createProfile(ProfileDto profileDto) throws RoleNotFoundException {
     	// Check if user with email already exists, since email is unique
+
+        //email is also username
+        profileDto.setUsername(profileDto.getEmail());
+
         if (profileRepository.findByEmail(profileDto.getEmail()).isPresent()) {
             throw new EmailInUseException();
         }
+        //encode the password
+        profileDto.setPassword(encoder.encode(profileDto.getPassword()));
 
-        List<Role> role;
-        if (profileDto.getRoles() == null || profileDto.getRoles().isEmpty()) {
-            Role defaultRole = roleRepository.findByName("USER")
-                    .orElseThrow(() -> new RoleNotFoundException("Default role not found"));
-            role = List.of(defaultRole);
-        } else {
-            role = findRoleByIds(profileDto.getRoles());
-        }
+        Set<String> strRoles = profileDto.getRoles();
+        Set<Role> roles = new HashSet<>();
+        createOrUpdateRolesForProfile(strRoles, roles);
 
-        // Encrypt the password 
-        //String encodedPassword = passwordEncoder.encode(password);
-	
         Profile profile = profileMapper.toEntity(profileDto);
-        profile.setRoles(role);
+        profile.setRoles(roles);
         Profile savedProfile = profileRepository.save(profile);
 
         return profileMapper.toDto(savedProfile);
+    }
+
+    private void createOrUpdateRolesForProfile(Set<String> strRoles, Set<Role> roles) throws RoleNotFoundException {
+        if (strRoles == null) {
+            Role userRole = roleRepository.findByName(RoleConstants.ROLE_USER)
+                    .orElseThrow(() -> new RoleNotFoundException("Default USER role not found"));
+            roles.add(userRole);
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "admin":
+                        Role adminRole = roleRepository.findByName(RoleConstants.ROLE_ADMIN)
+                                .orElseThrow(() -> new RuntimeException("Error: ADMIN Role is not found."));
+                        roles.add(adminRole);
+
+                        break;
+                    case "organiser":
+                        Role modRole = roleRepository.findByName(RoleConstants.ROLE_ORGANISER)
+                                .orElseThrow(() -> new RuntimeException("Error: ORGANISER Role is not found."));
+                        roles.add(modRole);
+
+                        break;
+                    default:
+                        Role userRole = roleRepository.findByName(RoleConstants.ROLE_USER)
+                                .orElseThrow(() -> new RuntimeException("Error: USER Role is not found."));
+                        roles.add(userRole);
+                }
+            });
+        }
     }
 
     @Override
@@ -86,8 +118,10 @@ public class ProfileServiceImpl implements ProfileService {
         if (profileDto.getName() != null && !Objects.equals(profileDto.getName(), existingProfile.getName())) {
             existingProfile.setName(profileDto.getName());
         }
+
         if (profileDto.getEmail() != null && !Objects.equals(profileDto.getEmail(), existingProfile.getEmail())) {
             existingProfile.setEmail(profileDto.getEmail());
+            existingProfile.setUsername(profileDto.getEmail());
         }
         if (profileDto.getConstituency() != null && !Objects.equals(profileDto.getConstituency(), existingProfile.getConstituency())) {
             existingProfile.setConstituency(profileDto.getConstituency());
@@ -95,10 +129,10 @@ public class ProfileServiceImpl implements ProfileService {
         if (profileDto.getPassword() != null && !profileDto.getPassword().isEmpty() ) {
             existingProfile.setPassword(profileDto.getPassword());
         }
-
-        // Update roles if provided
-        if (profileDto.getRoles() != null && !profileDto.getRoles().isEmpty()) {
-            List<Role> roles = findRoleByIds(profileDto.getRoles());
+        if (profileDto.getRoles() != null && !profileDto.getRoles().isEmpty()) {// Update roles if provided
+            Set<String> strRoles = profileDto.getRoles();
+            Set<Role> roles = new HashSet<>();
+            createOrUpdateRolesForProfile(strRoles, roles);
             existingProfile.setRoles(roles);
         }
 
@@ -117,24 +151,24 @@ public class ProfileServiceImpl implements ProfileService {
     }
 
 
-    @Override
-    public ProfileDto login(LoginRequestDTO loginRequestDTO) {
-    	// Check if user with email, check password
-    	
-    	Optional<Profile> profileOp = profileRepository.findByEmail(loginRequestDTO.getEmail());
-    	
-        if (profileOp.isPresent()) {
-            //check password
-        	if(!StringUtils.equals(profileOp.get().getPassword(), loginRequestDTO.getPassword())) {
-        		 throw new PasswordWrongException();
-        	}
-        }else {
-        	throw new UserNotExistedException();
-        } 
-        
-        return profileMapper.toDto(profileOp.get());
-
-    }
+//    @Override
+//    public ProfileDto login(LoginRequestDTO loginRequestDTO) {
+//    	// Check if user with email, check password
+//
+//    	Optional<Profile> profileOp = profileRepository.findByEmail(loginRequestDTO.getEmail());
+//
+//        if (profileOp.isPresent()) {
+//            //check password
+//        	if(!StringUtils.equals(profileOp.get().getPassword(), loginRequestDTO.getPassword())) {
+//        		 throw new PasswordWrongException();
+//        	}
+//        }else {
+//        	throw new UserNotExistedException();
+//        }
+//
+//        return profileMapper.toDto(profileOp.get());
+//
+//    }
 
     @Override
     public List<ProfileDto> getAllProfiles() {
