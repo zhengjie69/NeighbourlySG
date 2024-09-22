9be45ea390474dd2b69d330176235c674552f26e
Siow Jia Yie <jiayie.siow@ncs.com.sg>
2024-09-22 15:45:55 +0800

    LoginRequestDTO

--- neighbourlysg-backend/src/main/java/com/nusiss/neighbourlysg/dto/LoginRequestDTO.java
+++ neighbourlysg-backend/src/main/java/com/nusiss/neighbourlysg/dto/LoginRequestDTO.java
@@ -9,6 +9,14 @@ public class LoginRequestDTO {
 	@NotBlank
 	private String password;
 
+	public LoginRequestDTO() {
+	}
+
+	public LoginRequestDTO(String email, String password) {
+		this.email = email;
+		this.password = password;
+	}
+
 	public String getEmail() {
 		return email;
 	}
