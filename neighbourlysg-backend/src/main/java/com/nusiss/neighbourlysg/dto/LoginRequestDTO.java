7e75e1b7f93439d9e5ef3a13bf9606424a8b83fd
Siow Jia Yie <jiayie.siow@ncs.com.sg>
2024-10-11 16:43:07 +0800

    remove ununsed

--- neighbourlysg-backend/src/main/java/com/nusiss/neighbourlysg/dto/LoginRequestDTO.java
+++ neighbourlysg-backend/src/main/java/com/nusiss/neighbourlysg/dto/LoginRequestDTO.java
@@ -21,17 +21,8 @@ public class LoginRequestDTO {
 		return email;
 	}
 
-	public void setEmail(String username) {
-		this.email = username;
-	}
-
 	public String getPassword() {
 		return password;
 	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-    
     
 }
