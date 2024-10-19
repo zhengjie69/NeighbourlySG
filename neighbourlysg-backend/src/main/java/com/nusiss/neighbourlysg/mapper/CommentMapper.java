cbafc8700ea71da70160efab305a8915453c098c
clarkkohsj <clark.koh.2020@scis.smu.edu.sg>
2024-10-19 15:47:35 +0800

    Patching latest fixes/updates to dev

--- neighbourlysg-backend/src/main/java/com/nusiss/neighbourlysg/mapper/CommentMapper.java
+++ neighbourlysg-backend/src/main/java/com/nusiss/neighbourlysg/mapper/CommentMapper.java
@@ -10,9 +10,11 @@ public interface CommentMapper {
 
     @Mapping(source = "post.id", target = "postId")  // Map post ID
     @Mapping(source = "profile.id", target = "profileId")  // Map profile ID
+    @Mapping(source = "profile.name", target = "profileName") // Map profile Name
     CommentDto toDto(Comment comment);
 
     @Mapping(source = "postId", target = "post.id")  // Map post ID
     @Mapping(source = "profileId", target = "profile.id")  // Map profile ID
+    @Mapping(source = "profileName", target = "profile.name") // Map profile Name
     Comment toEntity(CommentDto commentDto);
 }
