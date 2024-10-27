f504aebedc1d935cacd69862593aee40c4d4fbf4
Lim Zheng Jie <zhengjie69@hotmail.com>
2024-10-28 00:42:25 +0800

    Change to all allow for testing

--- neighbourlysg-backend/src/main/java/com/nusiss/neighbourlysg/config/WebConfig.java
+++ neighbourlysg-backend/src/main/java/com/nusiss/neighbourlysg/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(webUrl) // Adjust as needed
+                .allowedOrigins("*") // Adjust as needed
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
